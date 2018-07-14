ba65fb6fe497d44c4f12d910232f9b90062ce559
Andrea Crotti <andrea.crotti.0@gmail.com>
2018-07-14 20:06:47 +0100

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to personal-website
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/personal_website/core_test.clj
@@ -0,0 +1,7 @@
+(ns personal-website.core-test
+  (:require [clojure.test :refer :all]
+            [personal-website.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
